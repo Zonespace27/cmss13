e2bcb11ec2f6a58d87a058e464a1fa1ce848426f
John Doe <johndoe@gmail.com>
2024-06-17 15:09:43 +0200

    quit being a bastard

--- tools/ci/install_auxtools.sh
+++ tools/ci/install_auxtools.sh
@@ -13,7 +13,7 @@ curl "https://github.com/$AUXTOOLS_REPO/archive/refs/tags/v$AUXTOOLS_VERSION.zip
 unzip libauxtools.zip
 rm libauxtools.zip
 cd libauxtools
-rustup target add i686-unknown-linux-gnu
+rustup -y target add i686-unknown-linux-gnu
 sudo dpkg --add-architecture i386
 sudo apt-get update
 sudo apt-get install build-essential g++-multilib libc6-i386 libstdc++6:i386
